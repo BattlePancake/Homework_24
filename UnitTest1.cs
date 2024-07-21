f20de7dab8deac9a2a4446bfe7c7e36136780500
BattlePancake <vladsivograkovwork@gmail.com>
2024-07-21 11:51:03 +0300

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Homework_24
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
